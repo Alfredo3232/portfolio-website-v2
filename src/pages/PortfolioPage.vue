4a88a1e03e32b3b3fe139e2dcf3cabd82c8b5ddc
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-11-24 11:21:32 -0600

    add PortfolioPage content

--- src/pages/PortfolioPage.vue
+++ src/pages/PortfolioPage.vue
@@ -1,3 +1,83 @@
 <template>
-  PortfolioPage
+  <div class="site-main">
+    <div class="site-main-inner">
+      <div class="main-section portfolio-section">
+        <h1 class="main-title">
+          Portfolio
+        </h1>
+
+        <p>
+          See my works below. Unless explicitly stated otherwise,
+          <strong>all their back-end and front-end parts were completely done by
+            me.</strong>
+        </p>
+
+        <div class="timeline portfolio-timeline">
+          <ul>
+            <li class="timeline_element project">
+              <div class="timeline_element-date">
+                <time class="timeline_element-date-text">2021-2022</time>
+              </div>
+
+              <div class="timeline_element-contents">
+                <div class="project-text">
+                  <a
+                    href="https://www.freecodecamp.org/AlfredoRojas"
+                    class="project-link"
+                  >
+                    <div class="project-title">Freecode Camp</div>
+                    <div class="project-subtitle">I completed various curriculums at FreeCodeCamp</div>
+                  </a>
+                  <div class="project-description">
+                    <p>
+                      DESCIPTION ON WHAT YOU DID ON FREECODECAMP AND WHAT YOUVE LEARNED.
+                    </p>
+                  </div>
+
+                  <div class="project-image">
+                    <img
+                      src="../images/FreeCodeCamp.png"
+                      alt="FreeCodeCamp Logo"
+                    >
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li class="timeline_element project">
+              <div class="timeline_element-date">
+                <time class="timeline_element-date-text">2019 - 2020</time>
+              </div>
+
+              <div class="timeline_element-contents">
+                <div class="project-text">
+                  <a
+                    href="https://www.calstatela.edu/"
+                    class="project-link"
+                  >
+                    <div class="project-title">Studied at California State University</div>
+                    <div class="project-subtitle">Studied at Cal State LA for a almost 2 years for a Major in Computer Science</div>
+                  </a>
+                  <div class="project-description">
+                    <p>
+                      I didn't complete my degree largely because I thought most of my time was spent on other topics
+                      that didnt intrest me.
+                      When I could be spending that time on learning what I actually love doing, and that is code. I
+                      really spent time alone studying a lot, learned more from reasearching it myself than I ever did from school.
+                    </p>
+                  </div>
+
+                  <div class="project-image">
+                    <img
+                      src="../images/logo.png"
+                      alt="Cal State LA Logo"
+                    >
+                  </div>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
