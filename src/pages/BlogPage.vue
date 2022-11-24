8c48c04065db60348111efcc1d839fabafefb8d6
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-11-24 17:07:08 -0600

    add BlogPage content and add ExampleBlog component

--- src/pages/BlogPage.vue
+++ src/pages/BlogPage.vue
@@ -1,3 +1,485 @@
 <template>
-  BlogPage
+  <router-view />
+
+  <div
+    id="__nuxt"
+    data-server-rendered="true"
+    class="site-main"
+  >
+    <div id="__layout">
+      <div class="wrapper">
+        <header>
+          <h1>Blogs</h1>
+        </header>
+        <main>
+          <div>
+            <div class="tags mb-6">
+              <hr size="30">
+              <button class="tag tag--active">
+                All
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                Beginner
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                Web Development
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                Computer Science
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                Programming
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                HTML
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                Python
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                JavaScript
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                TypeScript
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                CSS
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                GitHub
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                Git
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                TDD
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                Algorithms
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                Software Development
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                Open Source
+                <span class="number">?</span>
+              </button>
+
+
+              <hr size="30">
+              <button class="tag">
+                Front End
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                JQuery
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                Transpilation
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                React
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                Vue
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                Angular
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                Clean Code
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                SCSS
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                Tech
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                React Native
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                Linux
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                Node.js
+                <span class="number">?</span>
+              </button>
+
+              <hr size="30">
+              <button class="tag">
+                Backend
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                Productivity
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                MySQL
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                MongoDB
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                Fullstack
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                Project Management
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                Cloud
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                APIs
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                AWS
+                <span class="number">?</span>
+              </button>
+
+
+
+              <hr size="30">
+              <button class="tag">
+                Software Engineering
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                Freelancing
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                Docker
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                DevOps
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                Career
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                SEO
+                <span class="number">?</span>
+              </button>
+              <button class="tag">
+                Software Architecture
+                <span class="number">?</span>
+              </button>
+            </div>
+
+            <div class="blogs mb-6">
+              <div class="blog tag">
+                <div class="blog-header">
+                  <router-link
+                    to="/blog/ExampleBlog"
+                    class="ml-3 blog-link text-2xl"
+                  >
+                    How is Authentication done in most websites?
+                  </router-link>
+                </div>
+
+                <div class="blog-content">
+                  <div class="blog-description">
+                    Most of us just log in our favorite website and never really question how is it done in the backend?
+                    How can I still be logged in even if I close the browser?
+                  </div>
+                  <ul class="blog-tags">
+                    <li class="tag tag--small">
+                      Backend
+                    </li>
+                    <li class="tag tag--small">
+                      Cloud
+                    </li>
+                    <li class="tag tag--small">
+                      Fullstack
+                    </li>
+                    <li class="tag tag--small">
+                      Front End
+                    </li>
+                  </ul>
+                </div>
+
+                <div class="blog-footer">
+                  <a
+                    href=""
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    links...
+                  </a>
+                </div>
+              </div>
+
+              <div class="blog tag">
+                <div class="blog-header">
+                  <a
+                    href=""
+                    target="_blank"
+                    rel="noopener"
+                    class="ml-3 blog-link text-2xl"
+                  >
+                    Title of Blog
+                  </a>
+                </div>
+                <div class="blog-content">
+                  <div class="blog-description">
+                    Blog Description
+                  </div>
+                  <ul class="blog-tags">
+                    <li class="tag tag--small">
+                      Topics Talked About?
+                    </li>
+                  </ul>
+                </div>
+                <div class="blog-footer">
+                  <a
+                    href=""
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    links...
+                  </a>
+                </div>
+              </div>
+
+              <div class="blog tag">
+                <div class="blog-header">
+                  <a
+                    href=""
+                    target="_blank"
+                    rel="noopener"
+                    class="ml-3 blog-link text-2xl"
+                  >
+                    Title of Blog
+                  </a>
+                </div>
+                <div class="blog-content">
+                  <div class="blog-description">
+                    Blog Description
+                  </div>
+                  <ul class="blog-tags">
+                    <li class="tag tag--small">
+                      Topics Talked About?
+                    </li>
+                  </ul>
+                </div>
+                <div class="blog-footer">
+                  <a
+                    href=""
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    links...
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+
+        <footer class="text-center">
+          <p>
+            Made by <a href="https://portfolio-website-arojas32.herokuapp.com/">Alfredo Rojas</a>
+          </p>
+
+          <p>Code on <a href="https://github.com/arojas32/Portfolio-Website">GitHub</a>.</p>
+        </footer>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+button,
+input,
+optgroup,
+select,
+textarea {
+  font-family: inherit;
+  font-size: 100%;
+  line-height: 1.15;
+  margin: 0
+}
+
+.text-center {
+  text-align: center
+}
+
+.text-2xl {
+  font-size: 1.5rem
+}
+
+.mb-6 {
+  margin-bottom: 2rem
+}
+
+.ml-3 {
+  margin-left: 1rem
+}
+
+html {
+  --black: #0e141b;
+  --orange: #ff8a00;
+  --green: #4fc08d;
+  --pink: #a84fc0;
+  --rose: #ff0a78;
+  --purple: #7928ca;
+  --blue: #6ac;
+  --grey: #2a4555;
+  --dark-grey: #1e262f;
+  scroll-behavior: smooth;
+  color: #fff
+}
+
+html .wrapper {
+  padding: 2rem 2rem 0;
+  background-color: #333333;
+}
+
+html footer a {
+  text-decoration: underline
+}
+
+html .tag {
+  background-color: #333333;
+  background-color: var(--grey);
+  opacity: .8;
+  transform-origin: center center;
+  margin-right: 8px;
+  margin-bottom: 8px;
+  border: 0;
+  color: #fff;
+  cursor: pointer;
+  transition: all .4s;
+  padding: 5px;
+  border-radius: 5px;
+  font-size: 1.1rem;
+  outline: 0
+}
+
+html .tag:hover {
+  opacity: 1;
+  transform: scale(1.05)
+}
+
+html .tag--active {
+  background: #4fc08d;
+  background: var(--green)
+}
+
+html .tag--small {
+  cursor: default;
+  font-size: 1rem
+}
+
+html .tag--small:hover {
+  opacity: .8;
+  transform: none
+}
+
+html .tag .number {
+  background-color: #0e141b;
+  background-color: var(--black);
+  font-size: 1rem;
+  padding: 1px 3px;
+  border-radius: 2px
+}
+
+html .blogs {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+  grid-gap: 2rem;
+  gap: 2rem
+}
+
+html .blog {
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  background-color: #333333;
+  background-color: var(--dark-grey);
+  box-shadow: inset 0 1px 0 0 hsla(0, 0%, 100%, .1)
+}
+
+html .blog-header {
+  display: flex;
+  align-items: center;
+  padding: 15px
+}
+
+html .blog-content {
+  padding: 15px;
+  flex: 1;
+  display: flex;
+  flex-direction: column
+}
+
+html .blog-description {
+  font-size: 1.2rem;
+  line-height: 1.8rem;
+  flex: 1
+}
+
+html .blog-tags {
+  list-style-type: none;
+  padding: 0
+}
+
+html .blog-tags li {
+  display: inline-block
+}
+
+html .blog-footer {
+  display: flex;
+  border-top: 1px solid #2a4555;
+  border-top: 1px solid var(--grey)
+}
+
+html .blog-footer a {
+  width: 50%;
+  padding: 15px;
+  text-align: center;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis
+}
+</style>
---
+++ src/pages/blogContent/ExampleBlog.vue
@@ -0,0 +1,144 @@
+<template>
+  <main>
+    <div class="site-main">
+      <div class="main-section portfolio-section">
+        <header>
+          <h1>TITLE HOOK</h1>
+        </header>
+        <article>
+          <figure class="titleimg">
+            <img
+              src="/pix/dream_thinkpad_small.gif"
+              title="Image Description"
+            >
+            <figcaption>Image Header</figcaption>
+          </figure>
+
+          <p>Hook.</p>
+          <p>Hook transition to topic</p>
+          <h2>Title Question?</h2>
+          <p>Reasons if critizing or praising:</p>
+          <ol>
+            <li>reason1</li>
+            <li>reason2</li>
+            <li>reason2</li>
+            <li>reason3</li>
+          </ol>
+          <p>doubts of reasons?</p>
+          <p>describe your expirience with topic</p>
+          <h3>Question?</h3>
+          <p>Answer</p>
+          <h2>Title Reason 1</h2>
+          <h3>main answer one liner</h3>
+          <p>Explaination 1</p>
+          <p>Explaination 2, culprit?</p>
+          <h3>Statement why?</h3>
+          <h4>Reason</h4>
+          <p>
+            reason/history
+          </p>
+          <p />
+          <p>Note</p>
+          <h4>Specific topic or thing</h4>
+          <p>talk about what good or bad</p>
+          <h3> bigger specific topic or thing</h3>
+          <p>make the reader think with a question that stregthens your point</p>
+          <p>mock the topic or make light of it</p>
+          <p>main explaination</p>
+          <p>how can you resolve it or provide another point</p>
+          <h2>why the solution?</h2>
+          <p>question why do people like this solution?</p>
+          <p>point 1</p>
+          <p>point 2</p>
+          <h3>how good is one of your point to your soltion</h3>
+          <p>your expirience with the oposite</p>
+          <p>point 1</p>
+          <p>example to prove point</p>
+          <p>why solution is better than normal</p>
+        </article>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+:root {
+
+  --bg: #ccc;
+  --fg: #222;
+  --behind: #123;
+  --h1: #008;
+  --red: #400;
+  --yellow: #440;
+  --blue: #004;
+  --link: #037;
+  --linksec: #059;
+  --border: #333;
+  --box: #444;
+  --lime: #070;
+
+}
+
+body {
+  background: var(--behind);
+}
+
+.lw,
+p img,
+li img,
+h1 img,
+h2 img,
+h3 img,
+h4 img,
+dt img,
+td img,
+button img {
+  max-height: .8em;
+  max-width: 1em;
+  vertical-align: middle;
+}
+
+img {
+  max-width: 90%;
+}
+
+main {
+  max-width: 1000px;
+  margin: auto;
+}
+
+header h1 {
+  color: rgb(119, 215, 247);
+  border-style: solid;
+  border-width: 0px 0px 2px 0px;
+  border-color: #88c8ff;
+  text-align: center;
+  font-size: xx-large;
+  font-family: "Fira Sans", sans;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  text-align: center;
+}
+
+.titleimg {
+  align: center;
+}
+
+.titleimg img {
+  margin: auto;
+  max-width: 90%;
+  max-height: 400px;
+  display: block;
+}
+
+figcaption {
+  text-align: center;
+  display: block;
+}
+</style>
