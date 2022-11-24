16a8fd46f67a5d3391191eb9a1d900563537a3c5
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-11-24 12:06:41 -0600

    add content to SkillsPage

--- src/pages/SkillsPage.vue
+++ src/pages/SkillsPage.vue
@@ -1,3 +1,135 @@
 <template>
-  SkillsPage
+  <div class="site-main">
+    <div class="site-main-inner">
+      <div class="main-section offer-section">
+        <h1 class="main-title">
+          Skills
+        </h1>
+
+        <p>
+          <strong>A <span class="red">full-stack software engineer</span> with over
+            <span class="red">2 years of experience</span>.</strong>
+        </p>
+
+        <p>
+          <strong>I have worked on
+            <a
+              href="https://github.com/arojas32"
+            >multiple projects</a>
+            from scratch</strong>, working on both the back-end and the front-end.
+        </p>
+
+        <p>
+          My currrent skills includes
+          <strong>JavaScript, Node.js</strong> <strong>& React</strong>.
+        </p>
+
+        <p><strong>I can help you on your projects:</strong></p>
+        <ul>
+          <li>co-developing the back-end and front-end,</li>
+          <li>estimating tasks,</li>
+          <li>researching possible techs,</li>
+          <li>launching and monitoring the project.</li>
+        </ul>
+
+        <hr>
+
+        <h2 class="red">
+          <i class="fa fa-edit" />
+          Front-end Web Development
+        </h2>
+
+        <p>
+          <strong>I specialize in applications written in React.</strong>
+        </p>
+
+        <p>My current experience and skills in front-end includes:</p>
+        <ul>
+          <li>
+            JavaScript development: refactoring,
+            improving the structure, reliability, build setup;
+          </li>
+          <li>
+            experience in the JS ecosystem, using many libraries like React,
+            JQuery, Underscore.
+          </li>
+        </ul>
+
+        <div class="row features">
+          <div class="col col-4 feature">
+            <div class="feature-icon feature-number">
+              1
+            </div>
+            <div class="feature-description">
+              single page applications launched in React
+            </div>
+          </div>
+
+          <div class="col col-4 feature">
+            <div class="feature-icon feature-number" />
+            <div class="feature-description">
+              <!-- SOMETHING HERE -->
+            </div>
+          </div>
+
+          <!-- <div class="col col-4 feature">
+            <div class="feature-icon feature-number">
+              <a href="">
+                <img src="/images/" alt="" />
+              </a>
+            </div>
+            <div class="feature-description">
+              
+            </div>
+          </div> -->
+        </div>
+
+        <hr>
+
+        <h2 class="red">
+          <i class="fa fa-code" />
+          Back-end Web Development
+        </h2>
+
+        <p>
+          <strong>In back-end development, my current stack involves
+            <a
+              href="https://nodejs.org/"
+              rel="nofollow"
+            >NodeJS</a>
+          </strong>
+        </p>
+        <p>
+          The lessons I learned while doing all these project have been informational to me
+          all the projects.
+        </p>
+
+        <p>What I can do for the Back-End:</p>
+
+        <ul>
+          <li>development of the backends in Node.js,</li>
+          <li>
+            cooperation with APIs, remote data synchronizations, cloud servers,
+          </li>
+          <li>
+            using different types of databases like (
+            <a
+              href="https://www.mysql.com/"
+              rel="nofollow"
+            >MySQL</a>,
+            <a
+              href="https://www.mongodb.com/"
+              rel="nofollow"
+            >MongoDB</a>),
+          </li>
+          <li>
+            refactoring code and improving code readibility, by
+            separating logic into separate functions/classes/modules.
+          </li>
+        </ul>
+
+        <hr>
+      </div>
+    </div>
+  </div>
 </template>
