b4498fee09af434a32f9ce079f1bf139f3d940ce
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-11-24 12:11:17 -0600

    add content to ResumePage error: should isolate this component to have proper styling

--- src/pages/ResumePage.vue
+++ src/pages/ResumePage.vue
@@ -1,3 +1,159 @@
 <template>
-  ResumePage
+  <div class="cv-page h-resume">
+    <div class="cv-page-inner">
+      <div class="cv-section cv-header p-contact h-card">
+        <div class="cv-header-avatar">
+          <img src="/images/self.jpg">
+        </div>
+        <div class="cv-header-text">
+          <h1 class="cv-header-name p-name">
+            Alfredo Rojas
+          </h1>
+          <h2 class="cv-header-subname p-title">
+            Full-Stack Web Developer
+          </h2>
+          <div class="cv-header-lead p-note">
+            2 years of experience in the front-end and back-end of web development.<br>
+          </div>
+        </div>
+        <div class="cv-header-meta">
+          <div class="cv-header-meta-right">
+            <div class="cv-header-meta-row">
+              <a
+                href="https://portfolio-website-arojas32.herokuapp.com/"
+                rel="me"
+                class="u-url"
+              >https://portfolio-website-arojas32.herokuapp.com/</a>
+            </div>
+            <div class="cv-header-meta-row">
+              <p class="red">
+                Alfredo2001@me.com
+              </p>
+            </div>
+            <!-- <div class="cv-header-meta-row">
+              +48 792 290 484
+            </div> -->
+            <div class="cv-header-meta-row">
+              <a href="https://linkedin.com/in/AlfredoRojas1/">linkedin.com/in/AlfredoRojas1</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="cv-section">
+        <h3 class="cv-section-title">
+          Skills
+        </h3>
+        <div class="cv-section-content cv-section-content--indented">
+          <ul style="margin-top: 0">
+            <li>
+              Back-end: node.js, SQL databases, and NoSQL databases.
+            </li>
+            <li>
+              Front-end: JavaScript, React (with Hooks), JQuery, Underscore.
+            </li>
+            <li>
+              DevOps: configured and maintained web servers through AWS, Firebase, and Heroku.
+            </li>
+            <li>
+              Open to evolving in: Java, Python and many else.
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="cv-section">
+        <h3 class="cv-section-title">
+          Experience
+        </h3>
+        <div class="cv-section-content">
+          <div class="cv-timeline">
+            <div class="cv-timeline-row p-experience h-event">
+              <div class="cv-timeline-side">
+                <div class="cv-timeline-company">
+                  Project Title
+                </div>
+                <div class="cv-timeline-date">
+                  Date
+                </div>
+                <a
+                  href=""
+                  target="_blank"
+                  class="cv-timeline-website"
+                >Here goes a link to that project.</a>
+              </div>
+              <div class="cv-timeline-body p-summary">
+                <h4 class="cv-timeline-position p-name">
+                  Position
+                </h4>
+                <p>
+                  What did your project achieve?
+                </p>
+                <ul>
+                  <li>
+                    Discuss the different Technologies used.
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="cv-section">
+          <h3 class="cv-section-title">
+            Education
+          </h3>
+          <div class="cv-section-content">
+            <div class="cv-timeline">
+              <div class="cv-timeline-row p-education h-event">
+                <div class="cv-timeline-side">
+                  <div class="cv-timeline-company">
+                    California State University of Los Angeles
+                  </div>
+                  <div class="cv-timeline-date">
+                    2019 - 2020 (unfinished)
+                  </div>
+                  <a
+                    href="https://www.calstatela.edu/"
+                    target="_blank"
+                    class="cv-timeline-website"
+                  >Cal State LA</a>
+                </div>
+                <div class="cv-timeline-body p-summary">
+                  <div class="cv-timeline-position p-name">
+                    Computer Science
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="cv-section">
+          <h3 class="cv-section-title">
+            Other perks
+          </h3>
+          <div class="cv-section-content cv-section-content--indented">
+            <ul>
+              <li>
+                Knowledge of algorithms and data structures.
+              </li>
+              <li>
+                I can speak, write and read both English and Spanish fluently
+              </li>
+              <li>
+                In my free time I like to research about new upcoming Technologies, play video games and visit new
+                places.
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+</style>
