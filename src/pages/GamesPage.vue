1b5f40a96f9c1241934e2e5fcbe7b47c4a16eae8
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-11-24 12:08:36 -0600

    add content to GamesPage

--- src/pages/GamesPage.vue
+++ src/pages/GamesPage.vue
@@ -1,3 +1,17 @@
 <template>
-  GamesPage
+  <div class="site-main">
+    <div class="site-main-inner">
+      <div class="main-section">
+        <h1 class="main-title">
+          Games
+        </h1>
+
+        <p>Simple Games written in HTML, CSS, JavaScript.</p>
+
+        <div class="block-content">
+          CONTENT HERE
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
