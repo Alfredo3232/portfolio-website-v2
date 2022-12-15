f7269f58750a7ddce9ff1755ebd86174c4e4b0a6
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-12-14 20:26:43 -0600

    move some css from App to HomePage

--- src/App.vue
+++ src/App.vue
@@ -1847,7 +1847,6 @@ body {
 }
 
 .button.button-red,
-.home-section,
 .person,
 .project-subtitle,
 .project-title,
@@ -2056,15 +2055,6 @@ blockquote {
     margin-right: 1em
 }
 
-.button.button-outline {
-    border-width: 1px;
-    border-style: solid
-}
-
-.button.button-white {
-    color: #fff
-}
-
 .button.button-inline {
     padding: 0;
     margin-right: .3em
@@ -2180,7 +2170,6 @@ body {
 .cv-header-meta,
 .cv-section,
 .cv-timeline-row,
-.home-section .home-buttons,
 .post .post-meta,
 .row,
 .tech-icons li,
@@ -2663,134 +2652,11 @@ html:not(.platform-mac) .site-sidebar .block-blog {
     }
 }
 
-.home-section {
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    z-index: 1;
-    background: url(./images/home-bg3.ef171b05.jpg) center no-repeat #bababa;
-    background-size: cover;
-    color: #fff;
-    padding-left: 110px;
-    padding-top: 130px;
-    text-shadow:
-        0 0 7px #fff,
-        0 0 42px rgb(255, 0, 0),
-        0 0 102px rgb(255, 255, 255),
-        0 0 151px rgb(255, 0, 0);
-    font-size: 12px
-}
 
-@media screen and (max-width:1000px) {
-    .home-section {
-        position: relative;
-        padding: 40px 20px;
-        background: 0 0;
-        font-size: 10px
-    }
-}
 
 .tech-tags a,
 .tech-tags a:hover {
     background-color: rgba(191, 214, 201, 0.8)
-}
-
-@media screen and (max-width:767px) {
-    .home-section {
-        font-size: 8px
-    }
-}
-
-@media screen and (max-width:567px) {
-    .home-section {
-        font-size: 7px
-    }
-}
-
-.home-section .home-section-inner {
-    max-width: 840px;
-    margin-left: 25%;
-    padding-left: 50px;
-    background: rgba(0, 0, 0, 0.5)
-}
-
-@media screen and (max-width:1000px) {
-    .home-section .home-section-inner {
-        margin: 0
-    }
-}
-
-.home-section .home-heading {
-    font-size: 4em;
-    font-weight: 500
-}
-
-.home-section .home-subheading {
-    font-size: 3em;
-    font-weight: 300;
-    text-transform: none
-}
-
-.home-section .home-subheading strong {
-    font-weight: 500
-}
-
-.home-section .home-location {
-    font-size: 2em;
-    font-weight: 300;
-    margin-top: 1.25em
-}
-
-.home-section .home-buttons {
-    margin-top: 5.15em;
-    font-size: 2em;
-    -webkit-animation-name: fadein;
-    animation-name: fadein;
-    -webkit-animation-duration: 2s;
-    animation-duration: 2s;
-    -webkit-animation-iteration-count: 1;
-    animation-iteration-count: 1;
-    -webkit-animation-timing-function: ease-in-out;
-    animation-timing-function: ease-in-out
-}
-
-@media screen and (max-width:1000px) {
-    .home-section .home-buttons {
-        margin-top: 1em
-    }
-}
-
-.home-section .home-buttons .button {
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-    text-transform: uppercase;
-    font-weight: 300;
-    text-decoration: none;
-    opacity: .8;
-    box-shadow: 1px 1px 2px rgba(0, 0, 0, .47);
-    margin-bottom: 1em
-}
-
-@media screen and (max-width:767px) {
-    .home-section .home-buttons {
-        font-size: 18px
-    }
-
-    .home-section .home-buttons .button {
-        margin: 1em 0 0;
-        float: left;
-        clear: both
-    }
-}
-
-.home-section .home-buttons .button:active,
-.home-section .home-buttons .button:focus,
-.home-section .home-buttons .button:hover {
-    opacity: 1
 }
 
 .portfolio-section {
