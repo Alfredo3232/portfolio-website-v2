f126460fa33606b2ee6241eca3437439a141cc1d
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-02-04 14:06:48 -0800

    move css to pages

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -36,3 +36,31 @@
     </div>
   </div>
 </template>
+
+<style scoped>
+.dl-vertical dd,
+.dl-vertical dt,
+.main-navigation .navigation-extend-bar .social-icons,
+.pull-left {
+    float: left
+}
+
+.dl-vertical {
+    overflow: hidden
+}
+
+.dl-vertical dt {
+    display: block;
+    clear: both;
+    width: 150px
+}
+
+.dl-vertical dd {
+    display: block;
+    margin: 0
+}
+
+.dl-vertical.dl-long dt {
+    width: 350px
+}
+</style>
