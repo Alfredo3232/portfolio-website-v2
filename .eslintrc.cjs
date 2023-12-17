6208a0677185dcb2cc19c53e380511a50e0d2f0f
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-12-17 13:54:23 -0800

    Update dependencies Update eslint settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,67 +1,85 @@
-/* eslint-env node */
 module.exports = {
-  "env": {
-    "browser" : true,
-    "es2021"  : true,
-    "node"    : true
-  },
-  "extends": [
-    "plugin:vue/vue3-recommended",
-    "eslint:recommended"
-  ],
-  parserOptions: {
-    "ecmaVersion" : "latest",
-    "sourceType"  : "module"
-  },
-  root  : true,
-  rules : {
-    "arrow-body-style": [
-      "error",
-      "as-needed",
-      { "requireReturnForObjectLiteral": true }
+    "env": {
+        "browser" : true,
+        "es2021"  : true,
+        "node"    : true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/vue3-recommended"
     ],
-    "key-spacing": ["error", {
-      "align": {
-        "afterColon"  : true,
-        "beforeColon" : true,
-        "on"          : "colon"
-      }
-    }],
-    "no-console"           : "warn",
-    "no-debugger"          : "error",
-    "no-dupe-keys"         : "error",
-    "no-duplicate-imports" : "error",
-    "no-empty"             : "error",
-    "no-trailing-spaces"   : "error",
-    "no-unused-vars"       : "warn",
-    "no-var"               : "error",
-    "object-curly-newline" : ["error", {
-      "ExportDeclaration" : { "consistent": true, "minProperties": 3 },
-      "ImportDeclaration" : { "consistent": true, "minProperties": 3 },
-      "ObjectExpression"  : { "consistent": true, "minProperties": 3 },
-      "ObjectPattern"     : { "consistent": true, "minProperties": 3 }
-    }],
-    "prefer-destructuring": ["error", {
-      "array"  : true,
-      "object" : true
-    }],
-    "quotes": [
-      "error",
-      "double",
-      {
-        "allowTemplateLiterals" : true,
-        "avoidEscape"           : true
-      }
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
     ],
-    "semi"                                 : ["error", "always"],
-    "vue/component-definition-name-casing" : [
-      "error",
-      "kebab-case"
+    "parserOptions": {
+        "ecmaVersion" : "latest",
+        "sourceType"  : "module"
+    },
+    "plugins": [
+        "vue"
     ],
-    "vue/max-attributes-per-line": ["error", {
-      "singleline": {
-        "max": 3
-      }
-    }]
-  }
+    "rules": {
+        "arrow-body-style": [
+            "error",
+            "as-needed"
+        ],
+        "key-spacing": ["error", {
+            "align": {
+                "afterColon"  : true,
+                "beforeColon" : true,
+                "on"          : "colon"
+            }
+        }],
+        "no-console"           : "warn",
+        "no-debugger"          : "error",
+        "no-dupe-keys"         : "error",
+        "no-duplicate-imports" : "error",
+        "no-empty"             : "error",
+        "no-trailing-spaces"   : "error",
+        "no-unused-vars"       : "warn",
+        "no-var"               : "error",
+        "object-curly-newline" : ["error", {
+            "ExportDeclaration" : { "consistent": true, "minProperties": 3 },
+            "ImportDeclaration" : { "consistent": true, "minProperties": 3 },
+            "ObjectExpression"  : { "consistent": true, "minProperties": 3 },
+            "ObjectPattern"     : { "consistent": true, "minProperties": 3 }
+        }],
+        "quotes": [
+            "error",
+            "double",
+            {
+                "allowTemplateLiterals" : true,
+                "avoidEscape"           : true
+            }
+        ],
+        "comma-dangle": ["error", {
+            "arrays"    : "never",
+            "objects"   : "never",
+            "imports"   : "never",
+            "exports"   : "never",
+            "functions" : "never"
+        }],
+        "semi"                                 : ["error", "always"],
+        "eqeqeq"                               : ["error", "smart"],
+        "no-else-return"                       : "error",
+        "vue/component-definition-name-casing" : [
+            "error",
+            "kebab-case"
+        ],
+        "vue/max-attributes-per-line": ["error", {
+            "singleline": {
+                "max": 3
+            }
+        }]
+    }
 };
