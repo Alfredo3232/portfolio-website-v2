0873428710f6f47f937ed366a033e61caa500ac5
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2025-03-05 15:56:35 -0800

    Update portfolio info

--- src/pages/PortfolioPage.vue
+++ src/pages/PortfolioPage.vue
@@ -8,6 +8,7 @@
 
         <div class="timeline portfolio-timeline">
           <ul>
+
             <li class="timeline_element project">
               <div class="timeline_element-date">
                 <time class="timeline_element-date-text">2022 - 2022</time>
@@ -15,43 +16,46 @@
 
               <div class="timeline_element-contents">
                 <div class="project-text">
-                  <a
-                    href="https://www.linkedin.com/school/holacode/"
-                    class="project-link"
-                  >
+                  <a href="https://www.linkedin.com/school/holacode/" class="project-link">
                     <div class="project-title">
-                      Full-Stack Web Developer Teacher
+                      Web Development Teacher
                     </div>
 
                     <div class="project-subtitle">
-                      This was a momentary position that lasted 8 weeks. This
-                      was a collaboration between
-                      <a
-                        href="https://www.kanecode.com/"
-                        style="color: rgb(211, 20, 20)"
-                      >
+                      This was a momentary position that
+                      lasted 4 weeks. This was a
+                      collaboration between
+                      <a href="https://www.kanecode.com/" style="color: rgb(211, 20, 20)">
                         Kanecode
                       </a>
                       and
-                      <a
-                        href="https://www.linkedin.com/school/holacode/"
-                        style="color: rgb(211, 20, 20)"
-                      >
+                      <a href="https://www.linkedin.com/school/holacode/" style="color: rgb(211, 20, 20)">
                         HolaCode
                       </a>
                     </div>
                   </a>
                   <div class="project-description">
                     <p>
-                      • This was a collaboration between Kanecode and HolaCode,
-                      contracted by HolaCode. <br />
-                      • Taught an 8-week seminar on full-stack web development
-                      to students using Kanecode’s platform.<br />
-                      • Led students through various projects and exercises
-                      online. <br />
-                      • Provided personalized 1-on-1 sessions to help students
-                      with homework assignments and answer any questions they
-                      had<br />
+                      • Worked with Kanecode, contracted
+                      by HolaCode.
+                      <br />
+                      • Taught 3-week seminars on
+                      full-stack web development to
+                      students using Kanecode's platform.
+                      <br />
+                      • Focused on teaching JavaScript,
+                      HTML, CSS and JQuery and how to use
+                      them in conjunction with Kanecode's
+                      platform.
+                      <br />
+                      • Led students through various
+                      projects and exercises online.
+                      <br />
+                      • Provided personalized 1-on-1
+                      sessions to help students with
+                      homework assignments and answer any
+                      questions they had.
+                      <br />
                     </p>
                   </div>
 
@@ -67,33 +71,31 @@
 
               <div class="timeline_element-contents">
                 <div class="project-text">
-                  <a
-                    href="https://www.freecodecamp.org/Alfredo3232"
-                    class="project-link"
-                  >
+                  <a href="https://www.freecodecamp.org/Alfredo3232" class="project-link">
                     <div class="project-title">
                       Self-Studying & FreeCodeCamp
                     </div>
                     <div class="project-subtitle">
-                      I completed various curriculums at FreeCodeCamp
+                      I completed various curriculums at
+                      FreeCodeCamp
                     </div>
                   </a>
                   <div class="project-description">
                     <p>
-                      FreeCodeCamp was a starter in my full-stack web
-                      development that allowed me to ge started on a lot of
-                      things that I know now and the rest of the studying was
-                      looking at documentation and a lot of reading.
-                      Technologies like neo4j, vue, react, express node.js was
-                      from self studying.
+                      FreeCodeCamp was a starter in my
+                      full-stack web development that
+                      allowed me to ge started on a lot of
+                      things that I know now and the rest
+                      of the studying was looking at
+                      documentation and a lot of reading.
+                      Technologies like neo4j, vue, react,
+                      express node.js was from self
+                      studying.
                     </p>
                   </div>
 
                   <div class="project-image">
-                    <img
-                      src="../images/FreeCodeCamp.png"
-                      alt="FreeCodeCamp Logo"
-                    />
+                    <img src="../images/FreeCodeCamp.png" alt="FreeCodeCamp Logo" />
                   </div>
                 </div>
               </div>
@@ -108,19 +110,24 @@
                 <div class="project-text">
                   <a href="https://www.calstatela.edu/" class="project-link">
                     <div class="project-title">
-                      Studied at California State University
+                      Studied at California State
+                      University
                     </div>
                     <div class="project-subtitle">
-                      Studied at Cal State LA for a almost 2 years for a Major
-                      in Computer Science
+                      Studied at Cal State LA for a almost
+                      2 years for a Major in Computer
+                      Science
                     </div>
                   </a>
                   <div class="project-description">
                     <p>
-                      I didn't complete my degree, due to some circumstances at
-                      the time, this allowed me to learn the multiple things in
-                      life as well as the different types of technologies and
-                      paths that I can take in software development.
+                      I didn't complete my degree, due to
+                      some circumstances at the time, this
+                      allowed me to learn the multiple
+                      things in life as well as the
+                      different types of technologies and
+                      paths that I can take in software
+                      development.
                     </p>
                   </div>
 
@@ -130,6 +137,35 @@
                 </div>
               </div>
             </li>
+
+            <li class="timeline_element project">
+              <div class="timeline_element-date">
+                <time class="timeline_element-date-text">2019 - 2020</time>
+              </div>
+
+              <div class="timeline_element-contents">
+                <div class="project-text">
+                  <a href="https://www.collegiatecharterhighschooloflosangeles.org/" class="project-link">
+                    <div class="project-title">
+                      Studied at Collegiate Charter High School of Los Angeles
+                    </div>
+                    <div class="project-subtitle">
+                      Graduated, and got My High School Diploma
+                    </div>
+                  </a>
+                  <!-- <div class="project-description">
+                    <p>
+                      Something Here
+                    </p>
+                  </div> -->
+
+                  <div class="project-image">
+                    <img src="../images/collegiate.png" alt="Collegiate Charter High School Logo" />
+                  </div>
+                </div>
+              </div>
+            </li>
+
           </ul>
         </div>
       </div>
